2f4e9e5b198afafc4000726a265f4e22099989df
weep <ppcxy.com@gmail.com>
2017-08-11 13:40:19 +0800

    调整来自springside 的脚本中的showcase 字样为 cyfm

--- cyfm-web/bin/executable-war.sh
+++ cyfm-web/bin/executable-war.sh
@@ -8,6 +8,6 @@ set MAVEN_OPTS=%MAVEN_OPTS%
 
 mvn clean package -Pstandalone
 
-echo "[INFO] Executable war had been packaged as target/showcase-4.x.x.standalone.war"
+echo "[INFO] Executable war had been packaged as target/cyfm-1.x.x.standalone.war"
 
 cd bin
